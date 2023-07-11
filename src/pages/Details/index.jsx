0a2fca94f9b1d3b6206ed1da7fe62f8830df44b1
mthflashice <matheuslino@kerix.com.br>
2023-07-11 12:25:11 -0300

    Modified styles.css from styles.js

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,11 @@
-import './styles.css';
+import { Container} from './styles';
 
   export default function Details(){
 
   return(
-    <>
+    <Container>
   <h1>Hello World! </h1>
   <span> Matheus Quirino</span>
-  </>
+  </Container>
   )
 }
