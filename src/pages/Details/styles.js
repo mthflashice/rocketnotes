54d78962cfb4d804506f653f34af4d121ffcfb67
mthflashice <matheuslino@kerix.com.br>
2023-07-17 12:18:08 -0300

    add contend Section = index, styles

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -11,3 +11,15 @@ grid-template-areas:
 'content'
 `;
 
+export const Links = styled.ul`
+list-style-type: style none;;
+
+>li{
+    margin-top:12px;
+    a{
+      color: ${({theme})=>theme.COLORS.WHITE};
+
+    }
+}
+
+`;
