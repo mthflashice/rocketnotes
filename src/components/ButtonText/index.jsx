780344ae9e2e8eb756d8a6920f5c4fa6344f78bc
mthflashice <matheuslino@kerix.com.br>
2023-07-19 13:34:23 -0300

    Att isActive for ButtonText

--- src/components/ButtonText/index.jsx
+++ src/components/ButtonText/index.jsx
@@ -2,10 +2,11 @@
 import { Container } from "./styles";
 
 
-export  function ButtonText ({title, ...rest}){
+export  function ButtonText ({title,isActive=false, ...rest}){
     return(
         <Container
             type= 'button'
+            $isActive={isActive.toString()}
             {...rest}
         >
             {title}
