bae8ef7fa67699810e7b13cb0a942272919c7cbf
mthflashice <matheuslino@kerix.com.br>
2023-07-21 10:52:36 -0300

    add content home

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import{ThemeProvider} from 'styled-components';
-import Details from './pages/Details'
+import Home from './pages/Home'
 import GlobalStyle from './styles/global';
 import theme from  './styles/theme';
 
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme = {theme}>
       <GlobalStyle/>
-    <Details />
+    <Home />
     </ThemeProvider>
 
   </React.StrictMode>,
