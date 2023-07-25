359db5787a72585f33001c8d18e9c28d19c68518
mthflashice <matheuslino@kerix.com.br>
2023-07-25 11:49:30 -0300

    Create SignIn

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import{ThemeProvider} from 'styled-components';
-import Home from './pages/Home'
 import GlobalStyle from './styles/global';
 import theme from  './styles/theme';
+
+import SignIn from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme = {theme}>
       <GlobalStyle/>
-    <Home />
+    <SignIn />
     </ThemeProvider>
 
   </React.StrictMode>,
