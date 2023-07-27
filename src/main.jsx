3a668c7ece3da4a6e8e21ff92c5101d4107cfa68
mthflashice <matheuslino@kerix.com.br>
2023-07-27 11:53:06 -0300

    Create SignUp

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import{ThemeProvider} from 'styled-components';
 import GlobalStyle from './styles/global';
 import theme from  './styles/theme';
 
-import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme = {theme}>
       <GlobalStyle/>
-    <SignIn />
+    <SignUp />
     </ThemeProvider>
 
   </React.StrictMode>,
