ad13c071e7cdb1ba04440a8b5e6d4201acfe512e
mthflashice <matheuslino@kerix.com.br>
2023-07-31 12:37:57 -0300

    Create component Textarea

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import{ThemeProvider} from 'styled-components';
 import GlobalStyle from './styles/global';
 import theme from  './styles/theme';
 
-import Profile from './pages/Profile'
+import {New} from './pages/New'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme = {theme}>
       <GlobalStyle/>
-    <Profile />
+    <New />
     </ThemeProvider>
 
   </React.StrictMode>,
