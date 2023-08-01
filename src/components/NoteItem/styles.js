ab535c46ba5715b816c17d0bd705435fc6d57190
mthflashice <matheuslino@kerix.com.br>
2023-08-01 17:27:14 -0300

    correct

--- src/components/NoteItem/styles.js
+++ src/components/NoteItem/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
 
-  background-color: ${({ theme, isNew }) => isNew ? 'transparent' : theme.COLORS.BACKGROUND_900};
+  background-color: ${({ theme, isNew}) => isNew? "transparente" : theme.COLORS.BACKGROUND_900};
   color: ${({ theme }) => theme.COLORS.GRAY_300};
 
   
@@ -33,7 +33,7 @@ export const Container = styled.div`
     height:56px;
     width:100%;
 
-    padding: 12px;[
+    padding: 12px;
     
         color: ${({theme})=>theme.COLORS.White};
         background: transparent;
@@ -43,6 +43,6 @@ export const Container = styled.div`
         &::placeholder{
            color: ${({theme})=>theme.COLORS.GRAY_300};
         }
-    ]
+    
   }
 `;
