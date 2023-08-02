6bc4c931f86cc6411cdc74be87433a9d61f5c57b
mthflashice <matheuslino@kerix.com.br>
2023-08-02 10:56:19 -0300

    new content from paste 'new'

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,6 +3,8 @@ import{Input} from '../../components/Input'
 import{Textarea} from '../../components/Textarea'
 import{NoteItem} from '../../components/NoteItem'
 import{Section} from '../../components/Section'
+import{Button} from '../../components/Button'
+
 
 
 
@@ -29,6 +31,16 @@ export function New(){
                     <NoteItem isNew placeholder='Novo link'/>
 
                 </Section>
+                <Section title ='Marcadores'>
+                    <div className='tags'>
+                     <NoteItem value='react'/>
+                     <NoteItem isNew placeholder='Nova tag'/>
+                    </div>
+                    
+
+                </Section>
+
+                <Button title='Salvar'/>
 
 
                 </Form>
