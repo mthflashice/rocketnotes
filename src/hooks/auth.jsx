ab8d18c0ad959617ece1a34d8a2427904b519c4a
mthflashice <matheuslino@kerix.com.br>
2023-08-21 22:39:06 -0300

    Auth Provider

---
+++ src/hooks/auth.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext } from "react";
+
+export const AuthContext = createContext ({});
+
+
+function AuthProvider ({children}){
+    return (
+        <AuthContext.Provider value = {{name:'Matheus Quirino', email:'matheuslino@kerix.com.br'}}>
+            {children}
+        </AuthContext.Provider>
+
+    )
+}
+
+function useAuth(){
+    const context = useContext(AuthContext);
+
+    return context;
+}
+
+export {AuthProvider, useAuth};
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -4,9 +4,15 @@ import{Link} from 'react-router-dom'
 import{Input} from '../../components/Input'
 import{Button} from '../../components/Button'
 
+import {useAuth} from "../../hooks/auth";
+
 
 
 export  function SignIn(){
+
+    const data = useAuth();
+    console.log('Meu Contexto =>', data);
+    
     return(
         <Container>
             <Form>
