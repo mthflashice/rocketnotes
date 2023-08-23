e180bfc0d35c42725701f9e8baf1a681da7cc327
mthflashice <matheuslino@kerix.com.br>
2023-08-23 13:03:46 -0300

    localStorage add

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import {api} from '../services/api'
 
@@ -13,6 +13,10 @@ function AuthProvider ({children}){
             try{
              const response = await api.post('/sessions',{email,password});
              const{user, token} = response.data;
+
+             localStorage.setItem('@rocknotes:user', JSON.stringify(user));
+             localStorage.setItem('@rocknotes:token', token);
+
              
              api.defaults.headers.authorization = `Bearer ${token}`;
              setData({user, token})
@@ -25,8 +29,23 @@ function AuthProvider ({children}){
               alert('Não foi possível entrar')
              }
             }
-
          }    
+
+         useEffect (()=>{
+            const token = localStorage.getItem('@rocknotes:user');
+            const user = localStorage.getItem('@rocknotes:token');
+
+            if(token && user){
+                api.defaults.headers.authorization = `Bearer${token}`;
+
+                setData({
+                    token,
+                    user: JSON.parse(user )
+                });
+            }
+
+
+         },[]); //vetor 
 
     return (
         <AuthContext.Provider value = {{signIn, user: data.user}}>
