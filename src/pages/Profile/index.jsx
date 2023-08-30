907ae44f7dac48aff7627ab95ee8a441cec7853d
mthflashice <matheuslino@kerix.com.br>
2023-08-30 10:51:33 -0300

    add att Profile user

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import { Container,Form,Avatar } from "./styles";
 import {FiArrowLeft,FiMail, FiLock,FiUser,FiCamera} from 'react-icons/fi';
 import{Input} from '../../components/Input'
 import { Button } from "../../components/Button";
 import { Link } from "react-router-dom";
+import{useAuth} from '../../hooks/auth'
 
 
 
 export  function Profile(){
+    const {user}= useAuth();
+
+    const [name, setName] = useState(user.name);
+    const [email, setEmail] = useState(user.email);
+    const [passwordOld, setPasswordOld] = useState();
+    const [passwordNew, setPasswordNew] = useState();
+
     return(
         <Container>
             <header>
@@ -33,24 +42,33 @@ export  function Profile(){
             placeholder= 'Nome'
             type='texto'
             icon={FiUser}
+            value ={name}
+            onChange ={e=>setName(e.target.value) }
             />
 
             <Input
             placeholder= 'Email'
             type='text'
             icon={FiMail}
+            value ={email}
+            onChange ={e=>setEmail(e.target.value) }
+
             />      
 
             <Input
             placeholder= 'Senha atual'
             type='password'
             icon={FiLock}
+            onChange ={e=>setPasswordOld(e.target.value) }
+
             />   
 
             <Input
             placeholder= 'Nova Senha'
             type='password'
             icon={FiLock}
+            onChange ={e=>setPasswordNew(e.target.value) }
+
             />          
 
             <Button title='Salvar'/>
