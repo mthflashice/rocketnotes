5c20a7accbed3d5087f3cab76eb237592bae3fab
mthflashice <matheuslino@kerix.com.br>
2023-09-13 20:03:20 -0300

    function handle add tags

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -14,6 +14,9 @@ export function New(){
     const [links, setLinks ]= useState([]);
     const [newLink, setNewLink ]= useState('');
 
+    const [tags, setTags ]= useState([]);
+    const [newTag, setNewTag ]= useState('');
+
 
     function handleAddLink(){
         setLinks(prevState =>[...prevState, newLink]);
@@ -23,6 +26,12 @@ export function New(){
     function handleRemoveLink(deleted){
 
         setLinks(prevState => prevState.filter(link => link !== deleted));
+    }
+
+    function handleAddTag (){
+      setTags(prevState =>[...prevState, newTag]);
+      setNewTag('');
+
     }
 
 
@@ -60,8 +69,21 @@ export function New(){
                 </Section>
                 <Section title ='Marcadores'>
                     <div className='tags'>
-                     <NoteItem value='react'/>
-                     <NoteItem isNew placeholder='Nova tag'/>
+                      {                    
+                      tags.map((tag, index)=>(
+                        <NoteItem 
+                        key={String(index)}
+                        value={tag}
+                        onClick={()=>{ }}
+                        />
+                      ))
+                       }
+                     <NoteItem 
+                     isNew
+                     placeholder='Nova tag'
+                     onChange={e => setNewTag(e.target.value)}
+                     value ={newTag}
+                     onClick={handleAddTag}/>
                     </div>
                     
 
