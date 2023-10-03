870cdcf473efe734777c709b1d22c29ac81cb0e2
mthflashice <matheuslino@kerix.com.br>
2023-10-03 10:57:04 -0300

    Sign Out in component Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,12 +3,21 @@ import {useAuth} from '../../hooks/auth';
 import {api} from '../../services/api'
 import { Container, Profile, Logout } from "./styles";
 import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+import { useNavigate } from 'react-router-dom';
 
 
 
 
 export function Header(){
     const  {signOut, user} = useAuth();
+    const navigation = useNavigate();
+
+    function handleSignOut(){
+        navigation('/');
+        signOut();
+
+    };
+
 
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
@@ -25,7 +34,7 @@ export function Header(){
                 </div>
             </Profile>
 
-            <Logout onClick={signOut}>
+            <Logout onClick={handleSignOut}>
                 <RiShutDownLine/>
             </Logout>
 
